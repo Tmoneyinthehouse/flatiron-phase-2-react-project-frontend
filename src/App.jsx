cb89e4f411bef03db0f3aa186b543da24c5dacbe
Tyler Brown <tylerbrown487@gmail.com>
2023-09-28 16:04:52 -0400

    created Header component & removed starter coce from App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+//main components
+//header component
+//main component
+//footer component
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+     
+    </>
+  )
+}
+
+export default App
