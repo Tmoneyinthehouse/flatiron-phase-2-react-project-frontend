edfca5e4c232dddb3f2d1a94dc379d8ecc2a57c9
Tyler Brown <tylerbrown487@gmail.com>
2023-09-28 18:04:26 -0400

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <h1>My App</h1>
+                    <ul>
+                        <li><a href="#top">Go to top</a></li>
+                        <li><a href="#" target="_blank">Social Media Link 1</a></li>
+                        <li><a href="#" target="_blank">Social Media Link 2</a></li>
+                        <li><a href="#" target="_blank">Social Media Link 3</a></li>
+                    </ul>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
